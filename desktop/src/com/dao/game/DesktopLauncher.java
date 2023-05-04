bd7954823e18c9f3e747d70f85ee7e81d6626885
Aviv jan <avivjan2000@gmail.com>
2023-05-04 19:00:29 +0200

    add empty libGDX proejct

---
+++ desktop/src/com/dao/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.dao.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.dao.game.DaoGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Dao");
+		new Lwjgl3Application(new DaoGame(), config);
+	}
+}
